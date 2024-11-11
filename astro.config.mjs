8d45f1678c85f13b50121b6acf43be09d26343d9
theSaviKing <tpaulkariuki@gmail.com>
2024-11-10 20:36:32 -0600

    updated vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel()
+  output: 'static',
+  adapter: vercel({})
 });
