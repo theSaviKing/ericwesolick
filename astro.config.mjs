ba95d4f9c081fbd769ed6af397e7e744002a5bc4
theSaviKing <tpaulkariuki@gmail.com>
2025-02-18 17:29:29 -0600

    fixed broken LinkedIn icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 import favicons from "astro-favicons";
 
 // https://astro.build/config
